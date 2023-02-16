3f3ea34326a0bfdac9e27c6108be2cc8559162b7
Omer Cohen <omer@descope.com>
2023-02-16 14:50:23 +0200

    feat: switch to urllib3
    cloudflare sdk does not allow modifying httpadapter to control
    timeouts and retries so switching to urllib3 for full control

--- cf-purge/app.py
+++ cf-purge/app.py
@@ -1,17 +1,30 @@
 import os
-import CloudFlare
+import json
+import urllib3
 
-cf = CloudFlare.CloudFlare()
+http = urllib3.PoolManager()
 zone = os.environ.get("CLOUDFLARE_ZONE")
+api_token = os.environ.get("CLOUDFLARE_API_TOKEN")
 base_url = os.environ.get("BASE_URL")
+
 
 def lambda_handler(event, context):
     print("## EVENT: ", event)
-    for record in event['Records']:
+    for record in event["Records"]:
         purge(record["s3"]["object"]["key"])
 
 
 def purge(key):
     file = base_url + key
     print("## Purging ", file)
-    print(cf.zones.purge_cache.post(zone, data={'files': [file]}))
+    r = http.request(
+        "POST",
+        f"https://api.cloudflare.com/client/v4/zones/{zone}/purge_cache",
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {api_token}",
+        },
+        body=json.dumps({"files": [file]}),
+        timeout=1.0,
+    )
+    print(r.status, r.data, key)

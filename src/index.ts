e7b33864e70be2d3374b90a8f613150bde920766
Daniel Newman <129662919+newman-dani@users.noreply.github.com>
2025-06-30 14:49:46 +0300

    fix(lambda) - purge only EUC proejcts (#65)
    * fix(lambda) - purge only EUC proejcts
    
    * make it pass

--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,18 @@ export const handler = async (event: S3Event) => {
     if (!process.env[envVar]) throw new Error(`${envVar} is not set`);
   }
 
+  const key = path.dirname(event.Records[0].s3.object.key);
+
+  // Only purge if key contains "Peuc"
+  if (!key.includes("Peuc")) {
+    console.info({ message: 'Key does not contain "Peuc", skipping purge.', key });
+    return;
+  }
+
   const cloudflare = new Cloudflare({
     apiToken: process.env.CLOUDFLARE_API_TOKEN,
   });
 
-  const key = path.dirname(event.Records[0].s3.object.key);
   const prefix = path.join(process.env.BASE_URL!, key);
   console.info({
     message: "Purging cache",
